2a7fe9721aec2b97a797e5e716dc871a91f76153
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2020-12-14 12:46:58 -0600

    Day 14-A, 14-B

--- src/Common/Extensions.cs
+++ src/Common/Extensions.cs
@@ -65,5 +65,62 @@ namespace AdventOfCode2020.Common
                 throw new ArgumentOutOfRangeException(nameof(step), "step cannot be zero");
             }
         }
+
+        public static int CountTrailingZeros(this uint i)
+        {
+            if (i == 0) return 32;
+
+            uint y, n = 31;
+            y = i << 16; if (y != 0) { n &= ~16u; i = y; }
+            y = i << 8; if (y != 0) { n &= ~8u; i = y; }
+            y = i << 4; if (y != 0) { n &= ~4u; i = y; }
+            y = i << 2; if (y != 0) { n &= ~2u; i = y; }
+            return (int)(n - ((i << 1) >> 31));
+        }
+
+        public static int CountTrailingZeros(this ulong i)
+        {
+            if (i == 0) return 64;
+
+            uint x, y, n = 63;
+            y = (uint)i;
+            if (y != 0) { n &= ~32u; x = y; }
+            else x = (uint)(i >> 32);
+
+            y = x << 16; if (y != 0) { n &= ~16u; x = y; }
+            y = x << 8; if (y != 0) { n &= ~8u; x = y; }
+            y = x << 4; if (y != 0) { n &= ~4u; x = y; }
+            y = x << 2; if (y != 0) { n &= ~2u; x = y; }
+            return (int)(n - ((x << 1) >> 31));
+        }
+
+        public static int CountLeadingZeros(this uint i)
+        {
+            if (i == 0) return 32;
+
+            uint n = 1;
+            if (i >> 16 == 0) { n |= 16; i <<= 16; }
+            if (i >> 24 == 0) { n |= 8; i <<= 8; }
+            if (i >> 28 == 0) { n |= 4; i <<= 4; }
+            if (i >> 30 == 0) { n |= 2; i <<= 2; }
+            n -= i >> 31;
+            return (int)n;
+        }
+
+        public static int CountLeadingZeros(this ulong i)
+        {
+            if (i == 0) return 64;
+
+            uint n = 1;
+            uint x = (uint)(i >> 32);
+            if (x == 0) { n |= 32; x = (uint)i; }
+
+            if (x >> 16 == 0) { n |= 16; x <<= 16; }
+            if (x >> 24 == 0) { n |= 8; x <<= 8; }
+            if (x >> 28 == 0) { n |= 4; x <<= 4; }
+            if (x >> 30 == 0) { n |= 2; x <<= 2; }
+            n -= (x >> 31);
+            return (int)n;
+        }
     }
 }
---
+++ src/Days/14.cs
@@ -0,0 +1,164 @@
+﻿using AdventOfCode2020.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Days
+{
+    [Solution(14)]
+    class Day14 : ISolution
+    {
+        private const int Bits = 36;
+        private static readonly Regex MaskRegex = new Regex(@"^mask = ([0-1X]{36})$");
+        private static readonly Regex MemRegex = new Regex(@"^mem\[([0-9]+)\] = ([0-9]+)$");
+
+        private class InputLine 
+        { };
+
+        private class BitMask : InputLine
+        {
+            private ulong And { get; init; }
+            private ulong Or { get; init; }
+            private List<int> Floating { get; init; }
+
+            public BitMask(string mask)
+            {
+                And = Convert.ToUInt64(mask.Replace('X', '1'), 2);
+                Or = Convert.ToUInt64(mask.Replace('X', '0'), 2);
+                Floating = new List<int>();
+                for (int i = 0; i < mask.Length; ++i)
+                {
+                    if (mask[i] == 'X')
+                    {
+                        Floating.Add(i);
+                    }
+                }
+            }
+
+            public ulong ApplyToValue(ulong bits)
+            {
+                bits &= And;
+                bits |= Or;
+                return bits;
+            }
+
+            public IEnumerable<ulong> ApplyToAddress(ulong bits)
+            {
+                bits |= Or;
+                
+                // Unset all floating bits to start
+                foreach (int bit in Floating)
+                {
+                    bits &= ~(1UL << (Bits - 1 - bit));
+                }
+
+                yield return bits;
+
+                // Iterate using gray codes, toggling a bit at a time
+                int combinations = 1 << Floating.Count;
+                uint prev = 0;
+                for (int i = 1; i < combinations; ++i)
+                {
+                    // Next gray code
+                    uint grayCode = (uint)(i ^ (i >> 1));
+                    uint toggled = grayCode ^ prev;
+                    prev = grayCode;
+
+                    // Get the index of the bit that was shifted
+                    int bitIndex = toggled.CountTrailingZeros();
+
+                    // The next bit to toggle
+                    ulong nextBit = 1UL << (Bits - 1 - Floating[bitIndex]);
+
+                    // Unset the bit
+                    if ((grayCode & toggled) == 0)
+                    {
+                        bits &= ~nextBit;
+                    }
+                    // Set the bit
+                    else
+                    {
+                        bits |= nextBit;
+                    }
+
+                    yield return bits;
+                }
+            }
+        }
+
+        private class MemoryWrite : InputLine
+        {
+            public ulong Address { get; init; }
+            public ulong Value { get; init; }
+        }
+
+        private static List<InputLine> ParseInput(string input)
+        {
+            return input.Lines().Select<string, InputLine>(line =>
+            {
+                Match maskMatch = MaskRegex.Match(line);
+                if (maskMatch.Success)
+                {
+                    return new BitMask(maskMatch.Groups[1].Value);
+                }
+                else
+                {
+                    Match memMatch = MemRegex.Match(line);
+                    if (memMatch.Success)
+                    {
+                        return new MemoryWrite()
+                        {
+                            Address = uint.Parse(memMatch.Groups[1].Value),
+                            Value = uint.Parse(memMatch.Groups[2].Value)
+                        };
+                    }
+                }
+                throw new InputParseException();
+            }).ToList();
+        }
+
+        public object PartA(string input)
+        {
+            List<InputLine> lines = ParseInput(input);
+            Dictionary<ulong, ulong> memory = new Dictionary<ulong, ulong>();
+            BitMask currentMask = null;
+            foreach (InputLine line in lines)
+            {
+                if (line is BitMask mask)
+                {
+                    currentMask = mask;
+                }
+                else if (line is MemoryWrite memoryWrite)
+                {
+                    memory[memoryWrite.Address] = currentMask.ApplyToValue(memoryWrite.Value);
+                }
+            }
+            return memory.Values.Aggregate(0UL, (sum, value) => sum + value);
+        }
+
+        public object PartB(string input)
+        {
+            List<InputLine> lines = ParseInput(input);
+            Dictionary<ulong, ulong> memory = new Dictionary<ulong, ulong>();
+            BitMask currentMask = null;
+            foreach (InputLine line in lines)
+            {
+                if (line is BitMask mask)
+                {
+                    currentMask = mask;
+                }
+                else if (line is MemoryWrite memoryWrite)
+                {
+                    foreach (ulong address in currentMask.ApplyToAddress(memoryWrite.Address))
+                    {
+                        memory[address] = memoryWrite.Value;
+                    }
+                }
+            }
+            return memory.Values.Aggregate(0UL, (sum, value) => sum + value);
+        }
+    }
+}
